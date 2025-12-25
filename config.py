f07a01353be406bc6745410a0c7d69885aecaa0d
Techify Bots <TechifyBots@gmail.com>
2025-12-25 11:28:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,11 @@ from typing import List
 API_ID = os.environ.get("API_ID", "")
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "1255023013"))
 PICS = (os.environ.get("PICS", "")).split()
-
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
-
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002686843200"))
+NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "True").lower() == "true"
 DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "")
-
+DB_NAME = os.environ.get("DB_NAME", "autoapprovebot")
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
